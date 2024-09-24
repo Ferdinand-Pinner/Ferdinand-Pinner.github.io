5b6ad7dc41e8627a3575a316198840ecd01a9110
Ferdinand Pinner <ferdinand.pinner@gmail.com>
2024-09-24 09:59:28 -0400

    trying different publish action

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",  // <=== enables static exports
+    basePath: "/Ferdinand-Pinner.github.io",
     reactStrictMode: true,
+    images: {
+        unoptimized: true,
+      },
   };
 
 export default nextConfig;
