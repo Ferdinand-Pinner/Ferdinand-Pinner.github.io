fee0488be3c151dbce30c65a89a871a17ee78b31
Ferdinand Pinner <ferdinand.pinner@gmail.com>
2024-09-25 22:10:05 -0400

    removing base path from next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",  // <=== enables static exports
-    basePath: "/ferdinand-pinner.github.io",
-    assetPrefix: "/ferdinand-pinner.github.io",
     reactStrictMode: true,
     images: {
         unoptimized: true,
