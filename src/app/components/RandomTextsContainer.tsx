de10865e9c5fc7b58d106a59b1a15110841c2678
Ferdinand Pinner <ferdinand.pinner@gmail.com>
2024-12-17 18:37:25 -0500

    changing things up removing the random text as I want to add it back in the background

--- src/app/components/RandomTextsContainer.tsx
+++ src/app/components/RandomTextsContainer.tsx
@@ -1,14 +1,17 @@
 import React from "react";
-import RandomText from "./RandomText";
 
 const RandomTextsContainer: React.FC = () => {
-  const numberOfTexts = 4;
-
   return (
     <div className="random-texts-container">
-      {[...Array(numberOfTexts)].map((_, index) => (
-        <RandomText key={index} />
-      ))}
+      Hi! welcome to my little website, it&apos;s meant to be a little view into
+      my mind, a little manifestion of my mind if you will, I&apos;m an
+      engineer, a lover of the arts most things expressionist are my jam and I
+      love prog rock and math rock and I love philosophy. Always found it hard
+      to write about myself and say something at the same time, it&apos;s like
+      describing a box when you&apos;re inside it. So it&apos;s easier to talk
+      about what&apos;s going on and what&apos;s happening in the moment, but in
+      the moment you lose the box. That&apos;s kinda where I am in the moment,
+      in the box and forgetting I&apos;m there
     </div>
   );
 };
